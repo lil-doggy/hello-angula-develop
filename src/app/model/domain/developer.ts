e38e66fc92b082c105b4f40d82a7a879aaf979eb
lil-doggy <albino.caceres@gmail.com>
2026-09-12 14:23:45 -0500

    update grettings

---
+++ src/app/model/domain/developer.ts
@@ -0,0 +1,34 @@
+export class Developer {
+  readonly #id: string | null;
+  readonly #firstName: string;
+  readonly #lastName: string;
+  private static readonly MIN_NAME_LENGTH = 2;
+  private static readonly ANONYMOUS_NAME = 'Anonymous';
+
+  constructor(firstName: string= '', lastName: string) {
+    this.#firstName = firstName.trim();
+    this.#lastName = lastName.trim();
+  }
+get id():string | null {
+    return this.#id;
+  }
+  static isRegistered(developer: Developer): boolean {
+    return developer.#id !== null;
+  }
+
+  isRegistered(): boolean {
+    return Developer.isRegistered(this);
+  }
+
+  static isValidName(name: string): boolean {
+    return name.trim().length >= Developer.MIN_NAME_LENGTH;
+  }
+
+  static isValidForRegistration(firstName: string, lastName: string): boolean {
+    return Developer.isValidName(firstName) && Developer.isValidName(lastName);
+  }
+
+  get fullName(): string {
+    return !this.isRegistered()
+  }
+ }
